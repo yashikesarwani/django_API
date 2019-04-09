adb1a7f7ce1b0dd8f73d11c31f49e9ce72866d72
yashikesarwani <yashikesarwani092@gmail.com>
2019-04-09 18:01:23 +0530

    first commit

---
+++ web/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+
+
+
+def index(request):
+    return render(request, 'index.html')
+
+def face(request):
+    return render()
+
